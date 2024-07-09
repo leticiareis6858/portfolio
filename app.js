a9f206544be22073c8fab8d98b97483d34b25c62
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-07-09 01:10:35 -0300

    arruma o caminho dos gifs

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ function mudarTema() {
 
   // Parte para mudar o gif apresentado na home-page de acordo com o tema
   const gif = document.getElementById("gif");
-  const caminhoGIF = temaDark ? "/imagens/gray-cat.gif" : "/imagens/cat.gif";
+  const caminhoGIF = temaDark
+    ? "/portfolio/imagens/gray-cat.gif"
+    : "/portfolio/imagens/cat.gif";
 
   if (gif) {
     gif.src = caminhoGIF;
@@ -28,7 +30,9 @@ function aplicarTemaSalvo() {
 
   // Parte para mudar o gif apresentado na home-page de acordo com o tema
   const gif = document.getElementById("gif");
-  const caminhoGIF = temaDark ? "/imagens/gray-cat.gif" : "/imagens/cat.gif";
+  const caminhoGIF = temaDark
+    ? "/portfolio/imagens/gray-cat.gif"
+    : "/portfolio/imagens/cat.gif";
 
   if (gif) {
     gif.src = caminhoGIF;
